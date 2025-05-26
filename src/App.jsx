19a8e6294ad3cb9b54649e83916354310b73c0ec
Umrzoq993 <112821293+Umrzoq993@users.noreply.github.com>
2025-05-27 00:36:22 +0500

    queryid

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,12 @@ const App = () => {
         body: JSON.stringify(cartItems),
       });
     } else {
-      telegram.sendData(JSON.stringify(cartItems));
+      telegram.sendData(
+        JSON.stringify({
+          products: cartItems,
+          queryID: queryID,
+        })
+      );
     }
   }, [cartItems]);
 
